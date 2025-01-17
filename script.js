4acdd9a1bbbf9dcc1e05a207aeb9cfa859ce0f52
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-01-17 13:57:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function daysOfAYear(aYear) {
+	let isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+	return isLeapYear ? 366 : 365;
+}
+let year = prompt("Enter the days of a year");
+alert(daysOfAYear(year));
+
